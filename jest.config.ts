dac0d461db65f942e63136bc9631e87bb7f56f59
tizayi <57833318+tizayi@users.noreply.github.com>
2023-03-18 23:56:56 +0000

    lint

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
-import type {Config} from 'jest';
+import type { Config } from "jest";
 
 export default async (): Promise<Config> => {
   return {
     verbose: true,
-    preset: 'ts-jest',
-    testEnvironment: 'node',
+    preset: "ts-jest",
+    testEnvironment: "node",
   };
 };
--- tests/simpleHandlers.test.ts
+++ tests/simpleHandlers.test.ts
@@ -2,36 +2,36 @@ import { describe, expect, test, jest } from "@jest/globals";
 import { Message } from "discord.js";
 import Based from "../src/commands/based";
 import Ping from "../src/commands/ping";
-import Hello from "../src/commands/hello"
+import Hello from "../src/commands/hello";
 
 describe("Test simple command reply handlers", () => {
-    test("Test based command handler", async () => {
-        const message: Message = ({
-            channel: {send: jest.fn()},
-            reply: jest.fn()
-            } as unknown) as Message;
+  test("Test based command handler", async () => {
+    const message: Message = {
+      channel: { send: jest.fn() },
+      reply: jest.fn(),
+    } as unknown as Message;
 
-        Based.callback(message, "test");
-        expect(message.reply).toHaveBeenCalledWith("Not based") 
-    })
+    Based.callback(message, "test");
+    expect(message.reply).toHaveBeenCalledWith("Not based");
+  });
 
-    test("Test ping command handler", async () => {
-        const message: Message = ({
-            channel: {send: jest.fn()},
-            reply: jest.fn()
-            } as unknown) as Message;
-        Ping.callback(message);
+  test("Test ping command handler", async () => {
+    const message: Message = {
+      channel: { send: jest.fn() },
+      reply: jest.fn(),
+    } as unknown as Message;
+    Ping.callback(message);
 
-        expect(message.reply).toHaveBeenCalledWith("pong") 
-    })
+    expect(message.reply).toHaveBeenCalledWith("pong");
+  });
 
-    test("Test hello command handler", async () => {
-        const message: Message = ({
-            channel: {send: jest.fn()},
-            reply: jest.fn()
-            } as unknown) as Message;
-        Hello.callback(message);
+  test("Test hello command handler", async () => {
+    const message: Message = {
+      channel: { send: jest.fn() },
+      reply: jest.fn(),
+    } as unknown as Message;
+    Hello.callback(message);
 
-        expect(message.reply).toHaveBeenCalledWith("Welcome to robot hell.") 
-    })
-})
+    expect(message.reply).toHaveBeenCalledWith("Welcome to robot hell.");
+  });
+});
