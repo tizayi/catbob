4294892696f562e9677ccfffbf7032f0b78455a5
tizayi <57833318+tizayi@users.noreply.github.com>
2023-03-18 12:07:12 +0000

    working on steam command

--- src/commands/based.ts
+++ src/commands/based.ts
@@ -4,6 +4,7 @@ export default {
   callback: (message: Message, ...args: string[]): void => {
     const fullString = args.join("");
     const basedValue = hashCode(fullString);
+    console.log(basedValue);
     if (basedValue > 0) {
       message.reply("Not based");
     } else {
@@ -13,7 +14,7 @@ export default {
 };
 
 const hashCode = (input: string): number => {
-  return input.split("").reduce((a: number, b: string) => {
+  return input.split("").reduce((a: number, b: string): number => {
     a = (a << 5) - a + b.charCodeAt(0);
     return a & a;
   }, 0);
--- src/commands/steam.ts
+++ src/commands/steam.ts
@@ -1,18 +1,11 @@
 import { Message } from "discord.js";
 import axios from "axios";
 import * as dotenv from "dotenv";
-dotenv.config();
+import fs from "fs";
 
-export default {
-  callback: async (message: Message, ...args: string[]): Promise<void> => {
-    const gamesArray: number[][] = await Promise.all(args.map(getOwnedGames));
-    const result: number[] = gamesArray.reduce((a, b) =>
-      a.filter((c) => b.includes(c))
-    );
-    const gameNames: string[] = await Promise.all(result.map(getGamename));
-    message.reply(gameNames.join("\n"));
-  },
-};
+const USER_FILE_PATH = "./src/commands/steam-users/users.json"
+
+dotenv.config();
 
 interface steamGame {
   appid: number;
@@ -22,6 +15,52 @@ interface steamGame {
   playtime_linux_forever: number;
   rtime_last_played: number;
 };
+
+interface SteamUser {
+  name: string;
+  user_id: string
+}
+
+const userArrayWrite = (users: SteamUser[] = []): void => { 
+  fs.writeFileSync(USER_FILE_PATH, JSON.stringify(users));
+}
+
+const readUserArray = (): SteamUser[] => {  
+  const jsonString = fs.readFileSync(USER_FILE_PATH).toString();
+  return JSON.parse(jsonString);
+}
+
+const showUsers = (): string => {
+  const currentUsers = readUserArray();
+  return JSON.stringify(currentUsers)
+}
+
+const addUser = (newUser: SteamUser) => {
+  const currentUsers = readUserArray();
+  if(currentUsers.includes(newUser)){
+    return;
+  } else {
+    const newArray = [...currentUsers, newUser]
+    userArrayWrite(newArray);
+  }
+}
+
+const steamCompare = (users: SteamUser[]) => {
+
+}
+
+export default {
+  callback: async (message: Message, ...args: string[]): Promise<void> => {
+    console.log(readUserArray())
+    const gamesArray: number[][] = await Promise.all(args.map(getOwnedGames));
+    const result: number[] = gamesArray.reduce((a, b) =>
+      a.filter((c) => b.includes(c))
+    );
+    const gameNames: string[] = await Promise.all(result.map(getGamename));
+    message.reply(gameNames.join("\n"));
+  },
+};
+
 
 async function getOwnedGames(userId: string): Promise<number[]> {
   const response = await axios.get(
