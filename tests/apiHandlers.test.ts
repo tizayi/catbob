3d232dbce6ebc35b20c4c470ef5d6a701bdb1788
tizayi <57833318+tizayi@users.noreply.github.com>
2023-03-18 23:56:17 +0000

    adding unit tests

---
+++ tests/apiHandlers.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, test, jest } from "@jest/globals";
+import { Message } from "discord.js";
+import axios from "axios";
+import Rizz from "../src/commands/rizz";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe("Test rizz handler", () => {
+
+    test("Test test rizz api call", async () => {
+        const message: Message = ({
+            channel: {send: jest.fn()},
+            reply: jest.fn()
+        } as unknown) as Message;
+
+        const pickupLine = {data: {pickup: "Did it test when you fell from test"}}
+
+        mockedAxios.get.mockResolvedValue(pickupLine);
+
+        Rizz.callback(message, "test");
+        expect(mockedAxios.get).toHaveBeenCalledWith("https://vinuxd.vercel.app/api/pickup");
+    })
+})
---
+++ tests/simpleHandlers.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, test, jest } from "@jest/globals";
+import { Message } from "discord.js";
+import Based from "../src/commands/based";
+import Ping from "../src/commands/ping";
+import Hello from "../src/commands/hello"
+
+describe("Test simple command reply handlers", () => {
+    test("Test based command handler", async () => {
+        const message: Message = ({
+            channel: {send: jest.fn()},
+            reply: jest.fn()
+            } as unknown) as Message;
+
+        Based.callback(message, "test");
+        expect(message.reply).toHaveBeenCalledWith("Not based") 
+    })
+
+    test("Test ping command handler", async () => {
+        const message: Message = ({
+            channel: {send: jest.fn()},
+            reply: jest.fn()
+            } as unknown) as Message;
+        Ping.callback(message);
+
+        expect(message.reply).toHaveBeenCalledWith("pong") 
+    })
+
+    test("Test hello command handler", async () => {
+        const message: Message = ({
+            channel: {send: jest.fn()},
+            reply: jest.fn()
+            } as unknown) as Message;
+        Hello.callback(message);
+
+        expect(message.reply).toHaveBeenCalledWith("Welcome to robot hell.") 
+    })
+})
