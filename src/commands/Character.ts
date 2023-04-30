73905a1c5ce989528592a2390c9609b9f721c599
Treemode1999 <jacobbentley1999@hotmail.co.uk>
2023-04-30 12:22:49 +0100

    Character.ts

---
+++ src/commands/Character.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { Message } from "discord.js";
+import { Command } from "../utils";
+
+const command: Command = {
+  callback: async (message: Message, args: string[]): Promise<void> => {
+    axios
+    const classSelection = ["Barbarin", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rouge", "Sorcerer", "Warlock", "Wizard" ];
+    const class = Math.floor(Math.random() * classSelection.length);
+    const raceSelection = ["Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"];  
+    const Race = Math.floor(Math.random() * raceSelection.length); 
+    const strength = [(Random()*20)+1];
+    const dexterity = [(Random()*20)+1];
+    const constitution = [(Random()*20)+1];
+    const intelligence = [(Random()*20)+1];
+    const wisdom = [(Random()*20)+1];
+    const charisma = [(Random()*20)+1];
+    message.reply(race class /n "Strength:" strength " Dexterity:" dexterity " Constitution:" constitution " Intelligence:" intelligence " Wisdom:" wisdom " Charisma:" charisma);
+
+
+};
+  },
+  description: "Make a d and d character.",
+};
+
+export default command;
