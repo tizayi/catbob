dd07f3da6eafb3a7822b9c07b9a2c02bb66541b7
tizayi <57833318+tizayi@users.noreply.github.com>
2023-12-28 10:48:12 +0000

    remove things

--- tests/simpleHandlers.test.ts
+++ tests/simpleHandlers.test.ts
@@ -1,7 +1,6 @@
 import { describe, expect, test, jest } from "@jest/globals";
 import { Message } from "discord.js";
 import Based from "../src/commands/based";
-import Ping from "../src/commands/ping";
 import Hello from "../src/commands/hello";
 
 describe("Test simple command reply handlers", () => {
@@ -13,16 +12,6 @@ describe("Test simple command reply handlers", () => {
 
     Based.callback(message, ["test"]);
     expect(message.reply).toHaveBeenCalledWith("Cringe");
-  });
-
-  test("Test ping command handler", async () => {
-    const message: Message = {
-      channel: { send: jest.fn() },
-      reply: jest.fn(),
-    } as unknown as Message;
-    Ping.callback(message, []);
-
-    expect(message.reply).toHaveBeenCalledWith("pong");
   });
 
   test("Test hello command handler", async () => {
